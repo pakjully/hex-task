b4255b588e1754f306e863fa88086dcb7a765d53
Pak Yulia <kristalo4ka@gmail.com>
2024-02-15 23:04:21 +0300

    adding inputs validation

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
@@ -7,13 +7,48 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './LoginPage.scss';
 
+function validateRequireness(value) {
+  const error = value === '' ? 'Поле является обязательным' : '';
+  return error;
+}
+
+function validateLength(value) {
+  const error = value.length < 6 ? "Минимальная длина 6 символов" : '';
+  return error;
+}
+
+const validators = {
+  username: [validateRequireness],
+  password: [validateRequireness, validateLength],
+}
+
 export function LoginPage() {
   const navigate = useNavigate();
-  const [fields, setFields] = React.useState({
+  const [fields, setFields] = useState({
+    username: '',
+    password: '',
+  })
+  const [errors, setErrors] = useState({
     username: '',
     password: '',
   })
 
+  function validateField(name) {
+    const value = fields[name];
+    const fieldValidators = validators[name] ?? [];
+    const isFieldInvalid = fieldValidators.some((validator) => {
+      const validationResult = validator(value);
+      if (validationResult) {
+        setErrors((prevData) => ({
+          ...prevData,
+          [name]: validationResult,
+        }));
+      }
+      return Boolean(validationResult);
+    });
+    return isFieldInvalid;
+  }
+  
   function handleChange(e) {
     const { name, value } = e.target;
     setFields((prevState) => ({
@@ -23,7 +58,6 @@ export function LoginPage() {
   }
 
   function handleSubmit(e) {
-
     e.preventDefault();
     fetch('https://front-test.hex.team/api/login', {
       method: "POST",
@@ -48,6 +82,19 @@ export function LoginPage() {
         }
       })
   }
+
+  function handleFocus(e){
+    const { name } = e.target
+    setErrors((prevState) => ({
+      ...prevState,
+      [name]: '',
+    }))
+  }
+
+  function handleBlur(e) {
+    const { name } = e.target;
+    validateField(name);
+  }
   return(
     <div className="login">
       <p className="login-text">Войдите, чтобы получить доступ к сервису</p>
@@ -60,8 +107,11 @@ export function LoginPage() {
               name="username"
               type="text"
               placeholder="Логин"
-              required
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              isInvalid={errors.username}
             />
+            <Form.Control.Feedback type="invalid">{errors.username}</Form.Control.Feedback>
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3">
@@ -72,7 +122,11 @@ export function LoginPage() {
               name="password"
               type="password"
               placeholder="Пароль"
-              required/>
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              isInvalid={errors.password}
+            />
+            <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
           </Col>
         </Form.Group>
         <Form.Group as={Row} className="mb-3">
@@ -81,6 +135,7 @@ export function LoginPage() {
             <Button
               variant="primary"
               type="Submit"
+              disabled={fields.username === '' || fields.password.length < 6}
             >
           Войти
             </Button>
